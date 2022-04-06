f51cd50937138dc507a31da42cc3a3ed0846d3c4
Lacey Hande <laceyhan@pr-k3.kzn.21-school.ru>
2022-04-06 13:05:32 +0300

    1.

---
+++ src/max_num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, x, max = 0;
+
+    if ((scanf("%d", &a)) != 1) {
+        printf("n/a");
+    } else {
+        while (a != 0) {
+            x = a % 10;
+            a = a / 10;
+
+            if (x > max) {
+                max = x;
+            }
+        }
+        printf("%d", max);
+    }
+    return 0;
+}
