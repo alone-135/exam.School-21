3e4df0d0918a3f800535b1d6d8c0526efbe74811
Lacey Hande <laceyhan@pr-k3.kzn.21-school.ru>
2022-04-06 13:33:43 +0300

    05.

---
+++ src/sum_odd_num.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, x, sum_odd = 0;
+
+    if ((scanf("%d", &a)) != 1) {
+        printf("n/a");
+    } else {
+        while (a != 0) {
+            x = a % 10;
+            a = a / 10;
+
+            if (x % 2 != 0) {
+                sum_odd = sum_odd + x;
+            }
+        }
+        if (sum_odd == 0) {
+            printf("n/a");
+        } else {
+            printf("%d", sum_odd);
+        }
+    }
+
+    return 0;
+}
