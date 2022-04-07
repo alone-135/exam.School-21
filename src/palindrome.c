6240093fcf6d02dec6787596d4ecc59c972dd8d4
alone135 <alone.one.xyz@gmail.com>
2022-04-07 15:10:59 +0300

    add palindrome.c

---
+++ src/palindrome.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void change(char *word, int n);
+void check(const char *word, int n, int * check);
+
+int main() {
+    char word[100+1];
+    scanf("%100s", word);
+
+    int n = strlen(word);
+    int ch = 1;
+
+    change(word, n);
+    check(word, n, &ch);
+
+    if (ch == 1) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+
+    return 0;
+}
+
+void change(char* word, int n) {
+    for (int i = 0; i < n; i++) {
+        if (word[i] > 64 && word[i] < 91) {
+            word[i] += 32;
+        }
+    }
+}
+
+void check(const char *word, int n, int* ch) {
+    for (int i = 0; i < n / 2; i++) {
+        if (word[i] != word[n - i - 1]) {
+            *ch = 0;
+        }
+    }
+}
