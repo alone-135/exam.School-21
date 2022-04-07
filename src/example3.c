15dc888c372bc82a63cd6c6e04bf004b3667181b
Lacey Hande <laceyhan@pr-k3.kzn.21-school.ru>
2022-04-07 14:28:11 +0300

    ex3

--- src/example3.c
+++ src/example3.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void smena(char *word, int n);
-void proverka(const char *word, int n, int * check);
+void change(char *word, int n);
+void check(const char *word, int n, int * check);
 
 int main() {
-    char word[101];
-    scanf("%100s", word);
+    char word[1024+1];
+    scanf("%1024s", word);
     int n = strlen(word);
-    int check = 1;
+    int ch = 1;
 
-    smena(word, n);
-    proverka(word, n, &check);
+    change(word, n);
+    check(word, n, &ch);
 
-    if (check == 1) {
+    if (ch == 1) {
         printf("Yes");
     } else {
         printf("No");
@@ -23,7 +23,7 @@ int main() {
     return 0;
 }
 
-void smena(char* word, int n) {
+void change(char* word, int n) {
     for (int i = 0; i < n; i++) {
         if (word[i] > 64 && word[i] < 91) {
             word[i] += 32;
@@ -31,10 +31,10 @@ void smena(char* word, int n) {
     }
 }
 
-void proverka(const char *word, int n, int * check) {
-    for (int i = 0; i < n; i++) {
+void check(const char *word, int n, int* ch) {
+    for (int i = 0; i < n / 2; i++) {
         if (word[i] != word[n - i - 1]) {
-            *check = 0;
+            *ch = 0;
         }
     }
 }
