ea72aca382d004e5abbfc4fdeaaa607a58499143
Florentino Qarl <florentq@pr-k4.kzn.21-school.ru>
2022-04-07 14:36:34 +0300

    Create

--- src/example3.c
+++ src/example3.c
@@ -8,14 +8,13 @@ void check(const char *word, int n, int * check);
 int main() {
     char word[1024+1];
     scanf("%1024s", word);
-    
     int n = strlen(word);
     int ch = 1;
 
     change(word, n);
     check(word, n, &ch);
 
-    if(check == 1) {
+    if (check == 1) {
         printf("Yes");
     } else {
         printf("no");
@@ -27,8 +26,6 @@ int main() {
 void change(char* word, int n) {
     for (int i = 0; i < n; i++) {
         if (word[i] > 64 && word[i] < 91) {
-
-
             word[i] += 32;
         }
     }
