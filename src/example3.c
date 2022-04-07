e1f1f71a9d41e945e56f5cecd6175a22c2f688f1
Lacey Hande <laceyhan@pr-k3.kzn.21-school.ru>
2022-04-07 14:31:49 +0300

    example3.c

--- src/example3.c
+++ src/example3.c
@@ -8,6 +8,7 @@ void check(const char *word, int n, int * check);
 int main() {
     char word[1024+1];
     scanf("%1024s", word);
+    
     int n = strlen(word);
     int ch = 1;
 
@@ -26,6 +27,8 @@ int main() {
 void change(char* word, int n) {
     for (int i = 0; i < n; i++) {
         if (word[i] > 64 && word[i] < 91) {
+
+
             word[i] += 32;
         }
     }
